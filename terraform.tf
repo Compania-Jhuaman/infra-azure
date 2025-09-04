db9fd799a0bfab006d1c5050c115a90925a3601b
Jhuaman <jhuaman.huaman.huayllani@gmail.com>
2025-09-03 22:01:50 -0500

    cambios

--- terraform.tf
+++ terraform.tf
@@ -13,5 +13,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "b6dd1983-a328-4ef9-aae9-b401b616479d" # Id de suscripción
+  subscription_id = "b6dd1983-a328-4ef9-aae9-b401b616479d" # Id de suscripción.
 }
